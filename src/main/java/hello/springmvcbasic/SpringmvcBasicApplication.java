fab5f5c60cb68028793e975790514824d5e36594
Kai Kim <juhyeon1114@gmail.com>
2022-11-20 14:47:41 +0900

    init

---
+++ src/main/java/hello/springmvcbasic/SpringmvcBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvcbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvcbasic/SpringmvcBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvcbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
